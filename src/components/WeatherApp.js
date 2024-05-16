28b9402035c6bba6665d47cc07d86c9a7e8c84b3
Breno Ryan <brenoryan21@hotmail.com>
2024-05-15 23:45:15 -0300

    feat: Implementando consulta da cidade3

--- src/components/WeatherApp.js
+++ src/components/WeatherApp.js
@@ -1,11 +1,37 @@
-import React from 'react';
-import { View, Text, TextInput, Button } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, ActivityIndicator  } from 'react-native';
 import { styles } from '../styles/styles';
 
 const WeatherApp = () => {
-    const cityName = "New York"; // Exemplo de nome da cidade
-    const temperature = "25"; // Exemplo de temperatura
-    const wind = "10";
+
+    const [cityName, setCityName] = useState(""); // Estado para armazenar o nome da cidade
+    const [weatherData, setWeatherData] = useState(null);
+    const [loading, setLoading] = useState(false); 
+    const API_KEY = "a2f9cdc2498eebfc06947d967f787dd4"
+    const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`;
+
+    const handleSearch = () => {
+        if(!cityName){
+            alert("Digite o nome da cidade");
+            return;
+        }
+        setLoading(true);
+
+        fetch(apiUrl).then(response => {
+            if(!response.ok){
+                throw Error("Cidade não encontrada");
+            }
+            return response.json();
+        }).then(data => {
+            setWeatherData(data);
+            console.log(data);
+        }).catch(error => {
+            console.error("Erro ao buscar dados do clima:", error);
+            alert("Erro ao buscar dados do clima");
+        }).finally(() => {
+            setLoading(false)
+        })
+    };
     return (
         <View style={styles.container}>
             <View style={styles.content}>
@@ -15,9 +41,14 @@ const WeatherApp = () => {
                         style={styles.input}
                         placeholder="Digite o nome da cidade"
                         placeholderTextColor="#fff"
+                        onChangeText={(text) => setCityName(text)}
                     />
-                    <Button title="Buscar" style={styles.button} />
+                    <Button title="Buscar" style={styles.button} onPress={handleSearch} />
                 </View>
+                {loading && (
+                    <ActivityIndicator size="large" color="#fff" style={styles.loader} />
+                )}
+                {weatherData && (
                 <View style={styles.weatherInfoContainer}>
                     <Text style={styles.cityName}>{cityName}</Text>
                     <View style={styles.weatherDetails}>
@@ -25,10 +56,11 @@ const WeatherApp = () => {
                         <Text style={styles.wind}>Vento</Text>
                     </View>
                     <View style={styles.weatherDetails}>
-                        <Text style={styles.weatherLabel}>{temperature} <Text style={styles.degreeSymbol}>℃</Text></Text>
-                        <Text style={styles.weatherLabel}>{wind} <Text style={styles.degreeSymbol}>km/h</Text></Text>
+                        <Text style={styles.weatherLabel}>{weatherData.main.temp} <Text style={styles.degreeSymbol}>℃</Text></Text>
+                        <Text style={styles.weatherLabel}>{weatherData.wind.speed} <Text style={styles.degreeSymbol}>km/h</Text></Text>
                     </View>
                 </View>
+                )}
             </View>
         </View>
     );
